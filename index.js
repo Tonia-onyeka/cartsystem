6c06887be2f505ccfc6da9565d655b05a3d3b90e
Akwuohia Anthonia <anthoniaakwuohia@gmail.com>
2021-05-10 07:10:28 +0100

    myfirst commit

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const btn=document.getElementById("btn_link1")
-
-btn.addEventListener("click",()=>{
-    windows.location.replace("success.html")
-})
 
 /**ADD ITEM */
-const toaster = document.getElementById("toaster");
-const cartbtn = document.getElementById("cartbtn");
-cartbtn.addEventListener("click", () => {
-    toaster.className = "show";
-    setTimeout(() => { 
-        toaster.className = toaster.className.replace("show", "");
-    }, 3000);
-})
+
+
+
+
+
